e91100678b3d545d1c481452e084ec44fcc71486
Rien Coertjens <36336875+riencoertjens@users.noreply.github.com>
2023-06-08 11:52:43 +0200

    Update counter.tsx

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -7,18 +7,17 @@ export const Counter = component$(() => {
   const countVar = 11;
 
   return (
-    <div key={state.count}>
-      <h1>{$localize`Counter Example`} (with key attribute)</h1>
-      <div>{$localize`count: ${countVar}`} (without state)</div>
+    <div key={state.count}> {/* note `key` attribute */}
+      <h1>{$localize`Counter Example`} (with key attribute)</h1> {/* translates correctly */}
+      <div>{$localize`count: ${countVar}`} (without state)</div> {/* translates correctly */}
       <div>
-        {$localize`count: ${state.count}`}: {state.count}
+        {$localize`count: ${state.count}`}: {state.count} {/* doesn't translate on first render */}
       </div>
-      <div>{state.count > 1 ? `singular` : `plural`} (normal string)</div>
+      <div>{state.count > 1 ? `singular` : `plural`} (normal string)</div> {/* translated correctly */}
       <div>
-        {state.count > 1 ? $localize`singular` : $localize`plural`} ($localize
-        string)
+        {state.count > 1 ? $localize`singular` : $localize`plural`} ($localize string) {/* doesn't translate on first render */}
       </div>
-      <button onClick$={() => state.count++}>{$localize`increment`}</button>
+      <button onClick$={() => state.count++}>{$localize`increment`}</button> {/* translates correctly */}
     </div>
   );
 });
@@ -31,17 +30,16 @@ export const CounterWithoutKey = component$(() => {
 
   return (
     <div>
-      <h1>{$localize`Counter Example`} (without key attribute)</h1>
-      <div>{$localize`count: ${countVar}`} (without state)</div>
+      <h1>{$localize`Counter Example`} (without key attribute)</h1> {/* translates correctly */}
+      <div>{$localize`count: ${countVar}`} (without state)</div> {/* translates correctly */}
       <div>
-        {$localize`count: ${state.count}`}: {state.count}
+        {$localize`count: ${state.count}`}: {state.count} {/* doesn't translate or update on state change */}
       </div>
-      <div>{state.count > 1 ? `singular` : `plural`} (normal string)</div>
+      <div>{state.count > 1 ? `singular` : `plural`} (normal string)</div> {/* translated correctly */}
       <div>
-        {state.count > 1 ? $localize`singular` : $localize`plural`} ($localize
-        string)
+        {state.count > 1 ? $localize`singular` : $localize`plural`} ($localize string) {/* doesn't translate or update on state change */}
       </div>
-      <button onClick$={() => state.count++}>{$localize`increment`}</button>
+      <button onClick$={() => state.count++}>{$localize`increment`}</button> {/* translates correctly */}
     </div>
   );
 });
